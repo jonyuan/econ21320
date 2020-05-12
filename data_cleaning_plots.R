74c2aa9c10aece9534a77bf29b8d5e510f2e1e56
@eljeffew248 <jeffreywangca14@gmail.com>
2020-05-11 21:05:53 -0500

    Some more plots

--- data_cleaning_plots.R
+++ data_cleaning_plots.R
@@ -1,6 +1,8 @@
 library(readxl)
 library(ggplot2)
+library(plyr)
 library(dplyr)
+library(lubridate)
 
 
 ### Data Cleaning
@@ -17,17 +19,27 @@ cook_mobility$pubavg <- (cook_mobility$retail_and_recreation_percent_change_from
 
 cook_crimes$asdate <- as.Date(substr(as.character(cook_crimes$Date),0,9),"%m/%d/%y")
 crimesperday <- as.data.frame(table(cook_crimes$asdate))
+crimesperday$Date <- as.Date(x=crimesperday$Var1)
+
+### Making Crimes data into Weekly Averages:
+first <- as.Date("2020-01-11")
+last <- as.Date("2020-04-25")
+dt <- last-first
+crimesperday$week <- floor_date(crimesperday$Date,"week")+6
+crimes_weeklyavg <- ddply(crimesperday, .(week), function(z) mean(z$Freq))
+colnames(crimes_weeklyavg) <- c("week", "Average Crimes per Week")
 
 
-
-### Data Frame
+### Data Frames
 series <- data.table(date=c(seq(from = as.Date("2020-02-15"), to = as.Date("2020-05-01"), by='day')),
                      crimes_per_day=c(crimesperday$Freq[46:120],NA,NA),
                      mobility_perc_change=c(cook_mobility$pubavg,NA))
 
 wdata <- data.table(date=c(seq(from=as.Date("2020-01-04"), to = as.Date("2020-04-25"), by = 'week')),
-                    IL_UI_initclaims = c(IL_initclaims$`Initial Claims`[53:69]))
+                    IL_UI_initclaims = c(IL_initclaims$`Initial Claims`[53:69]),
+                    crimes_week = c(crimes_weeklyavg$`Average Crimes per Week`)[2:18])
 
+View(wdata)
 
 
 
@@ -57,8 +69,15 @@ ILUI_t <- ggplot(wdata,aes(x=date,y=IL_UI_initclaims))+
   ggtitle("IL State Weekly UI Initial Claims")
 
 crime_mob_scatter <- ggplot(series, aes(x=mobility_perc_change,y=crimes_per_day))+
-  geom_point()
+  geom_point()+
+  ggtitle("Crimes vs. Mobility Scatter")
 
+crime_UI_scatter <- ggplot(wdata, aes(x=IL_UI_initclaims, y=crimes_week))+
+  geom_point()+
+  ggtitle("Crimes vs. State_UI Scatter")
+
+crime_mob_scatter
+crime_UI_scatter
 
 
 
